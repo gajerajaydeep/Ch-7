fa0938dc60adba3a95e14afbf7fa8ae5d6244cf8
Jaydeep Gajera <jaydeepgajera63526@gmail.com>
2025-03-19 11:41:14 +0530

    Ch-7

---
+++ ch-7/src/App.jsx
@@ -0,0 +1,20 @@
+import Task_A from "./Ch-7-Tasks/TaskA/Task_A"
+import TaskB from "./Ch-7-Tasks/TaskB/TaskB";
+
+
+
+
+function App() {
+ 
+  return (
+    <>
+
+      {/* <Task_A/> */}
+    <TaskB/>
+    </>
+  )
+}
+
+export default App
+
+
