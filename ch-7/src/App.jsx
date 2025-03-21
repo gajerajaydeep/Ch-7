2f87f9b477f271488b8e8624d821c3141316f48e
Jaydeep Gajera <jaydeepgajera63526@gmail.com>
2025-03-21 15:20:31 +0530

    CH_7 Completed

--- ch-7/src/App.jsx
+++ ch-7/src/App.jsx
@@ -1,16 +1,30 @@
 import Task_A from "./Ch-7-Tasks/TaskA/Task_A"
 import TaskB from "./Ch-7-Tasks/TaskB/TaskB";
+import TaskC from "./Ch-7-Tasks/TaskC/TaskC";
+import TaskD from "./Ch-7-Tasks/TaskD/TaskD";
+import TaskE from "./Ch-7-Tasks/TaskE/TaskE";
+import TaskF from "./Ch-7-Tasks/TaskF/TaskF";
+import TaskG from "./Ch-7-Tasks/TaskG/TaskG";
+import TaskH from "./Ch-7-Tasks/TaskH/TaskH";
+import TaskJ from "./Ch-7-Tasks/TaskJ/TaskJ";
 
 
 
 
 function App() {
- 
+
   return (
     <>
 
       {/* <Task_A/> */}
-    <TaskB/>
+      {/* <TaskB/> */}
+      {/* <TaskC/> */}
+      {/* <TaskD/> */}
+      {/* <TaskE/> */}
+      {/* <TaskF /> */}
+      {/* <TaskG/> */}
+      {/* <TaskH/> */}
+   <TaskJ/>
     </>
   )
 }
